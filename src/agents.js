06d6c55a31268c8100c4643508af7367a846dd5f
rodmcnew <rodmcnew@gmail.com>
2017-05-15 21:21:33 -0500

    remove tabular sarsa

--- src/agents.js
+++ src/agents.js
@@ -2,19 +2,19 @@ import LookAhead from './agent/hand-programmed/LookAhead'
 import LookAheadIn5x3Viewport from './agent/hand-programmed/LookAheadIn5x3Viewport'
 // import MatrixDeepQNetwork from './agent/machine-learning/MatrixDeepQNetwork'
 import DeepQNetwork from './agent/machine-learning/DeepQNetwork'
-import TabularSARSA from './agent/machine-learning/TabularSARSA'
+// import TabularSARSA from './agent/machine-learning/TabularSARSA'
 
 export default [
     {
         name: 'ReinforcementLearning - DeepQNetwork - ranked - 212',
         class: DeepQNetwork
     },
-    {
-        class: TabularSARSA,
-        name: TabularSARSA.getName(),
-        description: TabularSARSA.getDescription(),
-        ticksPerInterval: 20000 //This agent runs fast but needs many games to learn
-    },
+    // {
+    //     class: TabularSARSA,
+    //     name: TabularSARSA.getName(),
+    //     description: TabularSARSA.getDescription(),
+    //     ticksPerInterval: 20000 //This agent runs fast but needs many games to learn
+    // },
     {
         name: 'HandProgrammed - LookAhead - ranked 255',
         class: LookAhead
