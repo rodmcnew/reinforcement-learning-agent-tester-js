e3bc701c252775b36dc4ab7426a59a22bbc29fc9
Rod Mcnew <rodmcnew@gmail.com>
2017-05-29 14:24:27 -0500

    make deep q learn faster via smaller viewport

--- src/agent/machine-learning/DeepQNetwork.js
+++ src/agent/machine-learning/DeepQNetwork.js
@@ -1,14 +1,14 @@
 import {matrixToFlatArray} from '../../environment/nestedFloatMatrixMath'
-import {data as savedNeuralNetwork} from '../../data/saves/deep-q-network-9x9'
-// import * as viewportConversions from '../../environment/viewportConversions'
+import {data as savedNeuralNetwork} from '../../data/saves/deep-q-network'
+import * as viewportConversions from '../../environment/viewportConversions'
 import Agent from '../../modules/deep-q-network/Agent'
 import {settings} from '../../App' //@TODO use DI instead for this
 import {renderActionResponse, renderReward} from './helper/qStateRenderer'
 import {actions, config} from '../../environment'
 import RewardCalculator from './helper/RewardCalculator'
 
-// const numberOfStates = 5*3;
-const numberOfStates = config.viewPortSize[0] * config.viewPortSize[1];
+const numberOfStates = 5 * 3;
+// const numberOfStates = config.viewPortSize[0] * config.viewPortSize[1];
 
 let agent = new Agent(numberOfStates, actions.length);
 let rewardCalculator = new RewardCalculator();
@@ -27,8 +27,8 @@ export default class MatrixDeepQNetwork {
      */
     getAction(observation) {
         const lastReward = rewardCalculator.calcLastReward(observation);
-        // const state = matrixToFlatArray(viewportConversions.convert9x9to5x3(observation.tileTypes));
-        const state = matrixToFlatArray(observation.tileTypes);
+        const state = matrixToFlatArray(viewportConversions.convert9x9to5x3(observation.tileTypes));
+        // const state = matrixToFlatArray(observation.tileTypes);
 
         // console.log(lastReward,state);
         let actionIndex = agent.learnAndAct(lastReward, state);
