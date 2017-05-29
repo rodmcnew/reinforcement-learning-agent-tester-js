48190f2fbf2ad75744a45a32b625d830617e3320
Rod Mcnew <rodmcnew@gmail.com>
2017-05-29 14:02:07 -0500

    add files

--- src/modules/deep-q-network/neural-network/NeuralNetwork.js
+++ src/modules/deep-q-network/neural-network/NeuralNetwork.js
@@ -1,22 +1,17 @@
 import DeepNetwork from 'layer-oriented-deep-learning-network-js/lib/network/DeepNetwork'
+import InputLayer from 'layer-oriented-deep-learning-network-js/lib/layer/InputLayer'
+import HiddenLayer from 'layer-oriented-deep-learning-network-js/lib/layer/HiddenLayer'
+import OutputLayer from 'layer-oriented-deep-learning-network-js/lib/layer/OutputLayer'
 import Linear from 'layer-oriented-deep-learning-network-js/lib/activation-function/Linear'
 import LeakyRelu from 'layer-oriented-deep-learning-network-js/lib/activation-function/LeakyRelu'
+import StochasticGradientDescent from 'layer-oriented-deep-learning-network-js/lib/optimizer/StochasticGradientDescent'
 export default function (inputCount, hiddenCount, outputCount) {
+    const learningRate = 0.01;
     return new DeepNetwork(
         [
-            { //Input layer
-                size: inputCount
-            },
-            { //Hidden layer
-                size: hiddenCount,
-                activationFunction: new LeakyRelu(),
-                learningRate: 0.01
-            },
-            { //Output layer
-                size: outputCount,
-                activationFunction: new Linear(),
-                learningRate: 0.01
-            }
+            new InputLayer(inputCount),
+            new HiddenLayer(hiddenCount, new LeakyRelu(), new StochasticGradientDescent(learningRate)),
+            new OutputLayer(outputCount, new Linear(), new StochasticGradientDescent(learningRate))
         ],
     );
 }
