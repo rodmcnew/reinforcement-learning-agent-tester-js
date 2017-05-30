a7053e7b595e0f754a86b81855534ca4eebda21e
rodmcnew <rodmcnew@gmail.com>
2017-05-30 18:52:45 -0500

    delete old matrix based neural network and q learning agent

--- src/old-unused-modules/matrix-deep-q-network/arrayMath.test.js
+++
@@ -1,4 +0,0 @@
-import * as arrayMath from './arrayMath'
-import {assertEquals} from './testHelper'
-
-assertEquals(arrayMath.getIndexOfMaxValue([1, 3, 4, 7, 2]), 3, 'arrayMath.getIndexOfMaxValue');
