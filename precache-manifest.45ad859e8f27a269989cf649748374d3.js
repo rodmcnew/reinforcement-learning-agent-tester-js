f17442b24db4d5a05ed56fd2db109daca9340d3b
Rod Mcnew <rodmcnew@gmail.com>
2021-09-16 00:20:36 -0500

    Commit final changes to broken branch

---
+++ precache-manifest.45ad859e8f27a269989cf649748374d3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bacded9befb4e67ba7d7a8e74e6cc4e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0a6f0dd1ad6e2233fecd",
+    "url": "./static/css/main.99787468.chunk.css"
+  },
+  {
+    "revision": "a7937b44d7f31ba27618",
+    "url": "./static/js/2.94a8d012.chunk.js"
+  },
+  {
+    "revision": "0a6f0dd1ad6e2233fecd",
+    "url": "./static/js/main.08285ad0.chunk.js"
+  },
+  {
+    "revision": "ef07b60769c5ed30a304",
+    "url": "./static/js/runtime-main.abf975a6.js"
+  }
+]);
